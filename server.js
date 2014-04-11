35faeef6e34195679a07aa8e1f2b8d46cfc9419e
useyourname <rhuq36@gmail.com>
2014-04-10 21:11:52 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+ var express = require("express");
+ var app = express();
+ 
+ /* serves main page */
+ app.get("/", function(req, res) {
+    res.sendfile('public/index.html')
+ });
+ 
+ var port = process.env.PORT || 5000;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
