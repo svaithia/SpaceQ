188b5b0eb4c2e3754e027dd696ece7e789540c21
Jerry Zhang <zhangjtxx@hotmail.com>
2014-04-12 16:50:02 -0400

    Populate player_class.js

--- server/model/player_class.js
+++ server/model/player_class.js
@@ -1,3 +1,7 @@
-var Person = function(){
+var Player = function(player_id, username){
+	this.id = player_id;
+	this.name = username;
+	this.cumulativeScore = 0;
 
+	this.updateCumulativeScore = function(incrementScore) { this.cumulativeScore += incrementScore; }
 }
