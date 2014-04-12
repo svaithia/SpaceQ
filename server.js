2c3adf35d05f8ca329c7d1418bd844e64929de59
Jerry Zhang <zhangjtxx@hotmail.com>
2014-04-12 13:39:15 -0400

    Delete js

--- server.js
+++ server.js
@@ -1,12 +1,56 @@
  var express = require("express");
  var app = express();
  
- /* serves main page */
- app.get("/", function(req, res) {
+/*// Mongoose import
+var mongoose = require('mongoose');
+
+// Mongoose connection to MongoDB (ted/ted is readonly)
+mongoose.connect('mongodb://ted:ted@ds061797.mongolab.com:61797/theenlighteneddeveloper', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
+// Mongoose Schema definition
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    id: int
+});
+
+// Mongoose Model definition
+var User = mongoose.model('users', UserSchema);
+
+// Bootstrap express
+var app = express();
+
+// URLS management
+
+app.get('/', function (req, res) {
+    res.send("<a href='/users'>Show Users</a>");
+});
+
+app.get('/users', function (req, res) {
+    User.find({}, function (err, docs) {
+        res.json(docs);
+    });
+});
+
+app.get('/users/:email', function (req, res) {
+    if (req.params.email) {
+        User.find({ email: req.params.email }, function (err, docs) {
+            res.json(docs);
+        });
+    }
+});
+*/
+/* serves main page */
+app.get("/", function(req, res) {
     res.sendfile('public/signin.html')
- });
+});
  
- var port = process.env.PORT || 5000;
- app.listen(port, function() {
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
    console.log("Listening on " + port);
- });
+});
