fac4bfb1e2858827a7df77180301101e2d848796
Jerry Zhang <zhangjtxx@hotmail.com>
2014-04-12 16:07:25 -0400

    Add createUserIfNoneExists in server.js

--- server.js
+++ server.js
@@ -1,50 +1,16 @@
- var express = require("express");
- var app = express();
- 
-/*// Mongoose import
+var express = require("express");
+var app = express();
+//require mongoose node module
 var mongoose = require('mongoose');
 
-// Mongoose connection to MongoDB (ted/ted is readonly)
-mongoose.connect('mongodb://ted:ted@ds061797.mongolab.com:61797/theenlighteneddeveloper', function (error) {
-    if (error) {
-        console.log(error);
-    }
+//connect to local mongodb database
+var db = mongoose.connect('mongodb://admin:admin@dbh75.mongolab.com:27757/nasahackathon');
+
+//attach lister to connected event
+mongoose.connection.once('connected', function() {
+	console.log("Connected to database")
 });
 
-// Mongoose Schema definition
-var Schema = mongoose.Schema;
-var UserSchema = new Schema({
-    first_name: String,
-    last_name: String,
-    id: int
-});
-
-// Mongoose Model definition
-var User = mongoose.model('users', UserSchema);
-
-// Bootstrap express
-var app = express();
-
-// URLS management
-
-app.get('/', function (req, res) {
-    res.send("<a href='/users'>Show Users</a>");
-});
-
-app.get('/users', function (req, res) {
-    User.find({}, function (err, docs) {
-        res.json(docs);
-    });
-});
-
-app.get('/users/:email', function (req, res) {
-    if (req.params.email) {
-        User.find({ email: req.params.email }, function (err, docs) {
-            res.json(docs);
-        });
-    }
-});
-*/
 /* serves main page */
 app.get("/", function(req, res) {
     res.sendfile('public/signin.html')
@@ -52,5 +18,17 @@ app.get("/", function(req, res) {
  
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-   console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
+
+function createUserIfNoneExists(player_id, username) {
+	db.players.findAndModify({
+		query: { id: player_id },
+		update: {
+			$setOnInsert: { id: player_id, name: username, cumulativeScore: 0 }
+		},
+		new: true,
+		upsert: true
+	})
+	console.log("player added to database");
+}
