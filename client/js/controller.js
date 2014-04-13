e70e0c979d376bab56a88a870dafec5dec962c03
Jerry Zhang <zhangjtxx@hotmail.com>
2014-04-13 00:23:24 -0400

    Marry views and controllers - AngularJS

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,3 +1,18 @@
-app.controller("MainController", function($scope){
-    $scope.inputValue = "";
+var gameApp = angular.module('gameApp', ['ngRoute']);
+
+gameApp.config(function ($routeProvider) {
+	$routeProvider
+		.when('/play',
+			{
+				controller: 'GameController',
+				templateUrl: 'game.html'
+			})
+		.otherwise({ redirectTo: '/' });
 });
+
+var controllers = {};
+controllers.GameController = function ($scope){
+    $scope.questions = 
+}
+
+gameApp.controller(controllers);
