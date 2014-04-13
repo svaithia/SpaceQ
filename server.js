5878f3ad0fe41c55ebbbd641f085e5ee7dc6d422
Jerry Zhang <zhangjtxx@hotmail.com>
2014-04-13 18:18:07 -0400

    Fix login screen bug

--- server.js
+++ server.js
@@ -98,9 +98,9 @@ io.sockets.on('connection', function(socket){
 			if(index != -1){
 				users.splice(index, 1);
 			}
-			var wait_list_index = wait_list.map(function(e) { return e.id; }).indexOf(socket.player);
+			var wait_list_index = wait_queue.map(function(e) { return e.id; }).indexOf(socket.player);
 			if(wait_list_index != -1){
-				wait_list.splice(wait_list_index,1);
+				wait_queue.splice(wait_list_index,1);
 			}
 
 		} else{
