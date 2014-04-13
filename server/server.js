834c1d4d5f98322f2bb8b1ed31a2351e3cb0d6b5
Jerry Zhang <zhangjtxx@hotmail.com>
2014-04-13 17:01:33 -0400

    Update controller.js

---
+++ server/server.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var app = express();
+//require mongoose node module
+var mongoose = require('mongoose');
+var player = require('./model/player_class');
+var path = require('path');
+
+var db = require('./database.js');
+// uncomment following line to see the twenty random questions in console
+// db.getQuestions(function(rightAnswersJsonArray, wrongAnswersJsonArray){}); 
+
+//use following to create a new player/update existing player
+// db.updatePlayerInfo({id:1, name: 'asdf', cumulativeScore: 123});  //NOTE: when testing remember to delete test entries after
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
+
+/* serves main page */
+app.get("/", function(req, res) {
+	var filepath = path.resolve('client/views/index.html');
+    res.sendfile(filepath);
+});
+
+app.get("/api/user/check_user_in_db/:id/:name", function(req, res){
+	var id = req.param('id');
+	var name = req.param('name');
+	
+	var player = require('./model/player_class'); 
+	var Player = new player.Player(id, name);
+	Player.createUserIfNoneExists();
+
+	console.log(player)
+	console.log(id + ' ' + name);
+	res.writeHeader(200, {"Content-Type": "text/plain"});
+	res.end(' Success message from server. : ' + id + ' ' + name);
+
+
+});
+
+// app.get('/hello', function(req,res){
+// 	var player = require('./model/player_class'); 
+// 	var Player = new player.Player('sfd', 'name');
+// 	Player.a();
+
+// 	res.writeHeader(200, {"Content-Type": "text/plain"});
+// 	res.end(' Successfully message from server');
+// });
+
+ 
+// app.get("/createUserIfNoneExist", function(req, res) {
+
+//     res.sendfile('../client/views/signin.html')
+// });
